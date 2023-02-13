33b3840bc928ad9cc6890298e2689b6bf0a9b147
srarai <yclass6@gmail.com>
2023-02-13 14:47:32 +0530

    mvctask7express

---
+++ routes/shop.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+const express = require('express');
+const productsController= require('../controllers/products');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
+const router = express.Router();
+
+router.get('/',productsController.getProducts);
+
+module.exports = router;
